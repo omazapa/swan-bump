36999b4921ad2ac965a3b37c2a0c80931f9b491b
Omar Zapata <Omar.Zapata@cern.ch>
2020-07-13 18:07:01 +0200

    added basic scripts to set version in setup.py and package.json, commit the changes, create a tag and push the tag to github

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do 
+  DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" 
+done
+
+SWAN_BUMP_DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+
+export PATH=$PATH:$SWAN_BUMP_DIR/bin
